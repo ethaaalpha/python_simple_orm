8b1cb59c5887fe8a8a47d553ca9202685a343e61
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-04-11 14:33:50 +0200

    started moved to static

--- src/mapper/StandardHolder.py
+++ src/mapper/StandardHolder.py
@@ -2,33 +2,39 @@ from abc import ABC
 from src.objects.StandardObject import StandardObject
 
 class StandardHolder(ABC):
-    def __init__(self, table_name: str):
-        self.__properties: dict[str, StandardObject] = {}
-        self.__table_name = table_name
-        self.__primary = []
+    __properties: dict[str, StandardObject] = {}
+    __table_name = None
+    __primary = []
 
-    def register_object(self, property_name: str, property_object: StandardObject, primary = False):
-        if self.__properties.get(property_name) != None:
+    def __new__(cls, table_name):
+        cls.__table_name = table_name
+        return super().__new__()
+
+    @classmethod
+    def register_object(cls, property_name: str, property_object: StandardObject, primary = False):
+        if cls.__properties.get(property_name) != None:
             raise NameError("Property name already existing")
         if len(property_name) == 0:
             raise NameError("Property musn't be empty")
 
-        self.__properties[property_name] = property_object
+        cls.__properties[property_name] = property_object
         if primary:
-            self.__primary.append(property_name)
+            cls.__primary.append(property_name)
 
-        self.__define_property(property_name)
+        cls.__define_property(property_name)
 
-    def get_definition(self) -> str:
-        query = f"CREATE TABLE IF NOT EXISTS `{self.__table_name}` ("
-        query += ', '.join(f"`{k}` {prop.get_sql()} NOT NULL DEFAULT '{prop.value_to_str()}'" for k, prop in self.__properties.items())
+    @classmethod
+    def get_definition(cls) -> str:
+        query = f"CREATE TABLE IF NOT EXISTS `{cls.__table_name}` ("
+        query += ', '.join(f"`{k}` {prop.get_sql()} NOT NULL DEFAULT '{prop.value_to_str()}'" for k, prop in cls.__properties.items())
         query += ", PRIMARY KEY (" 
-        query += ', '.join(f"`{prim}`" for prim in self.__primary)
+        query += ', '.join(f"`{prim}`" for prim in cls.__primary)
         query += "));"
 
         return query
 
-    def __define_property(self, property_name):
+    @classmethod
+    def __define_property(cls, property_name):
         """Create @property setter and getter for the property, use: self.property"""
         def getter(instance):
             return instance.__properties[property_name].value
@@ -36,4 +42,4 @@ class StandardHolder(ABC):
         def setter(instance, property_value):
             instance.__properties[property_name].value = property_value
 
-        setattr(self.__class__, property_name, property(getter, setter))
+        setattr(cls.__class__, property_name, property(getter, setter))
